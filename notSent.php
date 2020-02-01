23d104019dce795ae1596a9177d39bedcef64b45
Wasif Ali <wa537795@gmail.com>
2020-02-01 20:05:10 +0500

    Added ThankYou and Error pages

---
+++ notSent.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" href="thankyou.css" />
+<div class="top-container">
+  <h2>Oops! Seems like something went wrong!</h2>
+  <p>Make sure you filled form correctly</p>
+  <a id="goBack-btn" href="./index.html">Try Again!</a>
+</div>
--- sendEmail.php
+++ sendEmail.php
@@ -7,13 +7,19 @@ $subject = $_POST["subject"];
 $message = $_POST["message"];
 $subject = "You have recieved a message from " . $senderName ."\n". $subject;
 
-//mail($recieverEmail,$subject,$message);
+
 
  // Sending the message and all data to my email
 $myEmailAddress = "wasifali719@yahoo.com";
 $myMailSubject = "$senderName send an email to $recieverEmail with your app";
 $myEmail = "<h1>Here is the E-mail</h1> <br> " . "$message";
-//mail($myEmailAddress, $myMailSubject, $myEmail);
+mail($myEmailAddress, $myMailSubject, $myEmail);
 
-header("Location: ./thankYou.php");
+if (@mail($recieverEmail,$subject,$message))
+{
+    header("Location: ./thankYou.php");
+} else {
+    echo header("Location: ./notSent.php");
+}
+
 ?>
